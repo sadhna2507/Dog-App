5adbcf47d2140a0a43d7279e6ddc0233cb2fe8df
sadhna2507 <sadhnakhairia1998@gmail.com>
2022-12-17 19:18:03 +0530

    updated

--- src/compnents/BreedImage/DogBreedImage.jsx
+++ src/compnents/BreedImage/DogBreedImage.jsx
@@ -5,8 +5,10 @@ import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
 import { Button } from "@mui/material";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
+// import ImageList from "@mui/material/ImageList";
+// import ImageListItem from "@mui/material/ImageListItem";
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
 import axios from "axios";
 
 import "./DogBreedImage.css";
@@ -52,7 +54,21 @@ function DogBreedImage() {
         </Box>
 
         <Box textAlign="center" className="imgcont">
-          <ImageList
+
+        <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image={`${imageList}`}
+        alt="green iguana"
+      />
+      
+    </Card>
+
+
+
+
+          {/* <ImageList
             sx={{ width: "30%", height: "30%" }}
             cols={1}
             className="imgList"
@@ -67,7 +83,7 @@ function DogBreedImage() {
                 />
               </ImageListItem>
             </div>
-          </ImageList>
+          </ImageList> */}
         </Box>
       </Container>
     </div>
